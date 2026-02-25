659c008f5afe1f5e72765ee18ff4df9d7e1fcb97
ToltalTouch <micaeldoamaral@gmail.com>
2026-02-25 16:49:45 -0300

    feat: nova logica e otimização do codigo

--- InfoVagas.cs
+++ InfoVagas.cs
@@ -15,11 +15,42 @@ public class InfoVagas
         _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(120));
     }
 
-    public void ExibirInfoVagas()
+    private void HandleAlert()
     {
-        Console.WriteLine("Exibindo informações das vagas...");
-        _driver.Navigate().GoToUrl("https://www.infojobs.com.br/vagas-de-emprego-desenvolvedor-em-distrito-federal-trabalho-home-office.aspx");
+        try
+        {
+            Thread.Sleep(1000);
+            IAlert alert = _driver.SwitchTo().Alert();
+            Console.WriteLine($"Alerta encontrado com o texto: {alert.Text}");
+            alert.Accept();
+            Console.WriteLine("Alerta aceito.");
+        }
+        catch (NoAlertPresentException)
+        {
+            Console.WriteLine("Nenhum alerta presente.");
+        }
+        catch (WebDriverException ex)
+        {
+            Console.WriteLine($"Erro ao lidar com o alerta: {ex.Message}");
+        }
+    }
 
+    private void CookiesAlert()
+    {
+        try
+        {
+            IWebElement agreeButton = _wait.Until(d => d.FindElement(By.Id("didomi-notice-agree-button")));
+            Console.WriteLine("Aceitando cookies...");
+            agreeButton.Click();
+        }
+        catch (WebDriverTimeoutException)
+        {
+            Console.WriteLine("Botão de consentimento de cookies não encontrado ou tempo limite excedido.");
+        }
+    }
+
+    private void VagasLoop()
+    {
         var jobElements = _wait.Until(d => d.FindElements(By.XPath("//*[@id='filterSideBar']/div[1]/div[1]")));
         foreach (var jobElement in jobElements)
         {
@@ -38,5 +69,15 @@ public class InfoVagas
             }
 
         }
+    }
+
+    public void ExibirInfoVagas()
+    {
+        Console.WriteLine("Exibindo informações das vagas...");
+        _driver.Navigate().GoToUrl("https://www.infojobs.com.br/vagas-de-emprego-desenvolvedor-em-distrito-federal-trabalho-home-office.aspx");
+
+        HandleAlert();
+        CookiesAlert();
+        VagasLoop();
     }
 }
